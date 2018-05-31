f16fa9b942b19a65aa3718e5f0ee073c7fd49f09
Shubhra Mittal <shubhra.goel@gmail.com>
2018-05-31 14:07:18 -0700

    MongoDB scripts

--- userProjects.js
+++ userProjects.js
@@ -1,3 +1,4 @@
+
 use hgnData
 
 db.userProjects.drop()
@@ -8,12 +9,13 @@ db.createView('userProjects',
 {$unwind : "$projects"},
 {$project : {_id:1 , projects:1}},
 {$lookup : { from: "projects", localField: "projects", foreignField: "_id", as: "projectDetails" } },
-{$unwind: "$projectDetails"},
-{$project : {_id:1, projectId :"$projectDetails._id", projectName :"$projectDetails.projectName", tasks : "$projectDetails.tasks"}},
-{$group : {_id : "$_id", projects : {$addToSet :{projectId : "$projectId", projectName : "$projectName", tasks : "$tasks"} }}}
-
+{$unwind: {path : "$projectDetails", preserveNullAndEmptyArrays: true}},
+{$match : {"projectDetails.isActive": true}},
+{$project : {_id:1, projectId :"$projectDetails._id", projectName :"$projectDetails.projectName"}},
+{$sort : {_id: 1, projectName : -1}},
+{$group : {_id : "$_id", projects : {$addToSet :{projectId : "$projectId", projectName : "$projectName"} }}},
+{$unwind : "$projects"},
+{$sort : {_id : 1, "projects.projectName" : 1}},
+{$group : {_id : "$_id", projects : {$push :"$projects" }}},
 
 ])
-
-
-
